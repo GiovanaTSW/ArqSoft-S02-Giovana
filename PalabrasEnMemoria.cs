a027856c74f4c8b11f05ddd15792d4c51a665030
GiovanaTSW <giovana.diaz@tecdesoftware.edu.mx>
2026-05-12 12:41:12 -0600

    refactor: separar en IRepositorioPalabras y PalabrasEnMemoria (2)

---
+++ PalabrasEnMemoria.cs
@@ -0,0 +1,16 @@
+﻿namespace Ahorcado
+{
+    public class PalabrasEnMemoria : IRepositorioPalabras
+    {
+        private readonly List<string> _palabras = new()
+        {
+            "arquitectura", "interfaz", "polimorfismo", "encapsulamiento", "herencia", "alienigena", "tilapia"
+        };
+
+        public string ObtenerPalabraAleatoria()
+        {
+            var random = new Random();
+            return _palabras[random.Next(_palabras.Count)];
+        }
+    }
+}
