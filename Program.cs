50b0e01f15016e79e6e10159d1a032405520f419
GiovanaTSW <giovana.diaz@tecdesoftware.edu.mx>
2026-05-12 12:47:23 -0600

    refactor: limpiar Program.cs con inyección de dependencias

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿var juego = new Ahorcado.Juego();
-juego.Jugar();
+﻿using Ahorcado;
+
+var repositorio = new PalabrasEnMemoria();
+bool continuar = true;
+
+while (continuar)
+{
+    var motor = new MotorAhorcado(repositorio);
+    var ui = new ConsolaUI(motor);
+
+    Console.WriteLine("=== AHORCADO ===");
+
+    while (!motor.Ganado() && !motor.Perdido())
+    {
+        ui.MostrarTablero();
+        char letra = ui.PedirLetra();
+
+        if (motor.LetraYaUsada(letra))
+        {
+            ui.MostrarMensaje("Ya usaste esa letra.");
+            continue;
+        }
+
+        motor.RegistrarLetra(letra);
+    }
+
+    ui.MostrarTablero();
+
+    if (motor.Ganado())
+        ui.MostrarMensaje($"\n¡Ganaste! La palabra era: {motor.PalabraSecreta}");
+    else
+        ui.MostrarMensaje($"\nPerdiste. La palabra era: {motor.PalabraSecreta}");
+
+    continuar = ui.PreguntarOtraVez();
+}
